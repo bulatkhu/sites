1621ebbee2452852eb4fd25b4d5487b75d8abce8
BulatKhudainazarov <56464596+BulatKhudainazarov@users.noreply.github.com>
2020-02-27 17:09:54 +0200

    changes

--- js-slider/js-mySlider-X2/src/App.js
+++ js-slider/js-mySlider-X2/src/App.js
@@ -1,23 +1,31 @@
 const boxes = document.querySelectorAll('.block')
+const spaceOfSlider = document.querySelector('.box-slider')
 
-const boxesArr = []
+const boxArr = []
 
-for (let i = 0; i < boxes.length; i++) {
-  boxesArr[i] = boxes[i]
+for(let i = 0; i < boxes.length; i++) {
+  boxArr[i] = boxes[i]
   boxes[i].remove()
 }
+
+spaceOfSlider.appendChild(boxArr[0])
+boxArr[1].style.top = '150px'
+spaceOfSlider.appendChild(boxArr[1])
 
 let step = 0
 let offset = 0
 
-function draw() {
-  const boxSpace = document.querySelector('.box-slider')
-  boxesArr[step].style.top = ''
-  boxesArr[step + 1].style.top = '150px'
-  boxSpace.appendChild(boxesArr[step])
-  boxSpace.appendChild(boxesArr[step + 1])
+function click() {
+  const blocks = document.querySelectorAll('.block')
+  console.log(blocks[0], blocks[1])
+  blocks[step].style.top = '-150px'
+  blocks[step + 1].style.top = 0
+  setTimeout(() => {
+    blocks[step].remove()
+    spaceOfSlider.appendChild(boxArr[step + 2])
+    boxArr[step + 2].style.top = '150px'
+    step++
+  },1000)
 }
 
-document.onclick = draw
-
-console.log(boxesArr)
+document.onclick = click
